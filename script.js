a2547f4e6f4a3e7bb7fb952df0b19bc701e0660d
Botikangwapo <jervinvillones6@gmail.com>
2025-06-19 20:05:07 +0800

    Add script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+const toggle = document.getElementById('toggle');
+const menuToggle = document.getElementById('menu-toggle');
+const toggleBtn = document.getElementById('toggle-dark');
+const prefersDark = localStorage.getItem('dark-mode') === 'enabled';
+
+toggle.addEventListener('change', () => {
+  document.body.classList.toggle('dark-mode', toggle.checked);
+});
+
+
+
+  AOS.init({
+         duration: 1000,
+      });
+
+
+if (prefersDark) {
+  document.body.classList.add('dark-mode');
+}
+
+toggleBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  localStorage.setItem('dark-mode', document.body.classList.contains('dark-mode') ? 'enabled' : 'disabled');
+});
+
